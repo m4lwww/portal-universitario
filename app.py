f9a050e78c81492f78226437a9ae8738352195dc
malwwwwww <jimenez.malw16@gmail.com>
2026-09-24 23:15:39 -0600

    Feature: endpoint /api/status

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return f"<h1>Bienvenido al portal universitario, {nombre}!</h1>"
 
+@app.route('/api/status')
+def status():
+    return {"status": "ok", "entorno": "contenedor-docker", "version": "1.1.0"}
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
